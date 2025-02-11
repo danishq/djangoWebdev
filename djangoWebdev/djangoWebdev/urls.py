99856214ae0d218fd9480ce77fb95628730f32a7
danishq <dqureshi08@gmail.com>
2025-02-11 14:46:23 +0530

    made one and two apps

--- djangoWebdev/djangoWebdev/urls.py
+++ djangoWebdev/djangoWebdev/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name ='home'),
+    path('', views.home, name ='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('appOne/', include('appOne.urls')), 
